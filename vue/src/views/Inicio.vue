23884ded1122e46c0df18fbb3c2fe29a6c6a11b2
Raphael <2003094@aluno.univesp.br>
2021-09-23 21:33:05 -0300

    Página Início implementada.

--- vue/src/views/Inicio.vue
+++ vue/src/views/Inicio.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    Inicio
+  <div class="container vh-100">
+    <div class="row h-100 align-items-center">
+      <div class="col text-center">
+        <img src="@/assets/images/brasao_prefeitura.jpg"
+             alt="Brasão da Prefeitura Municipal de Penápolis">
+
+        <h1>Controle de Pedidos</h1>
+
+        <h3 class="mb-4">Prefeitura Municipal de Penápolis</h3>
+
+        <router-link tag="button" class="btn btn-primary" :to="{name: 'login'}">
+          <i class="bi bi-box-arrow-in-right"></i> Entrar na Aplicação
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,7 +22,3 @@ export default {
   name: 'Inicio',
 };
 </script>
-
-<style scoped>
-
-</style>
