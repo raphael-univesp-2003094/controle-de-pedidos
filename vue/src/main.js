4af557e38a03f523ff8dc2296faf804df8b7a8a1
Raphael <2003094@aluno.univesp.br>
2021-09-24 07:00:15 -0300

    Implementado interceptador axios para definir automaticamente, em toda requisição à API, o cabeçalho 'Authorization' contendo o token de acesso, caso o mesmo esteja salvo no armazenamento local, e caso o cabeçalho ainda não esteja definido.

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,10 +1,31 @@
 import Vue from 'vue';
+import axios from 'axios';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 // Importa os arquivos de estilo da aplicação.
 import './styles/app.scss';
+
+// Define um interceptador de requisições para o axios, que configura automaticamente o cabeçalho
+// 'Authorization' caso haja um token de acesso salvo no armazenamento local e caso o cabeçalho
+// ainda não esteja definido.
+axios.interceptors.request.use((config) => {
+  // Faz uma cópia da configuração da requisição.
+  const newConfig = config;
+
+  // Busca no armazenamento local o token de acesso.
+  const accessToken = localStorage.getItem('accessToken');
+
+  // Define o cabeçalho 'Authorization' caso haja um token de acesso salvo no armazenamento local e
+  // caso o cabeçalho ainda não esteja definido.
+  if (accessToken && !config.headers.Authorization) {
+    newConfig.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  // Retorna a cópia da configuração da requisição, ja reconfigurada.
+  return newConfig;
+});
 
 // Inicializa a instância da aplicação Vue.
 new Vue({
