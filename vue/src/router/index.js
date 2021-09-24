27b886c5094a1e68e0f16ed84558339295e98b98
Raphael <2003094@aluno.univesp.br>
2021-09-23 21:32:42 -0300

    Página Login criada.

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Inicio from '../views/Inicio.vue';
+import Login from '../views/Login.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Inicio',
+    name: 'inicio',
     component: Inicio,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
   },
 ];
 
