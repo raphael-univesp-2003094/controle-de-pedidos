f554f1eaa804933997f8b982a033a247b2034d50
Raphael <2003094@aluno.univesp.br>
2021-09-23 21:34:29 -0300

    Implementado carregamento do módulo de estado de autenticação.

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,3 +1,30 @@
 <template>
-  <router-view/>
+  <div>
+    <div v-if="isLoading">Carregando aplicação...</div>
+    <router-view v-else/>
+  </div>
 </template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  name: 'App',
+
+  computed: {
+    ...mapGetters({
+      isLoading: 'auth/isLoading',
+    }),
+  },
+
+  methods: {
+    ...mapActions({
+      initAuth: 'auth/init',
+    }),
+  },
+
+  mounted() {
+    this.initAuth();
+  },
+};
+</script>
