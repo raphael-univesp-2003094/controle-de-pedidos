959f3664f6efd66fccddda601d7e3506a9d6ff77
Raphael <2003094@aluno.univesp.br>
2021-09-25 09:54:00 -0300

    Pequenas modificações para compatibilidade com o Heroku.

--- src/__init__.py
+++ src/__init__.py
@@ -24,7 +24,7 @@ def create_app() -> Flask:
     # Configura a aplicação Flask.
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('SECRET_KEY'),
-        SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL'),
+        SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1),
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
         JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY'),
         JWT_ERROR_MESSAGE_KEY='error',
