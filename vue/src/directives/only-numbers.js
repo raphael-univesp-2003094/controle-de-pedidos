1313fc25a7ec3c19873d5e8c0b305ec6509d2927
Raphael <2003094@aluno.univesp.br>
2021-09-26 17:38:19 -0300

    Diretivas only-numbers (permite apenas números no input) e uppercase (altera o valor do input para maiúsculas) implementadas.

---
+++ vue/src/directives/only-numbers.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-param-reassign */
+
+/**
+ * Diretiva que permite que o valor do input seja composto apenas por números.
+ *
+ * @type {{
+ * inserted(HTMLInputElement): void
+ * }}
+ */
+const onlyNumbers = {
+  /**
+   * Vincula um event listener que é acionado ao pressionar uma tecla no input e ao colar algo da
+   * área de transferência, impedindo que seja inseridos textos que não são números no input.
+   *
+   * @param {HTMLInputElement} el
+   */
+  inserted(el) {
+    el.onkeypress = (event) => {
+      if (Number.isNaN(Number.parseInt(event.key, 10))) {
+        event.preventDefault();
+        return false;
+      }
+
+      return true;
+    };
+
+    el.onpaste = (event) => {
+      const data = event.clipboardData.getData('text');
+
+      for (let i = data.length - 1; i >= 0; i -= 1) {
+        const charCode = data.charCodeAt(i);
+        if (charCode < 48 || charCode > 57) {
+          event.preventDefault();
+          return false;
+        }
+      }
+
+      return true;
+    };
+  },
+};
+
+export default onlyNumbers;
